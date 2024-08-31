f92f3b04ad9d4b76e562884533c89b0c362751d8
Phuoc <phamhbphuoc@gmail.com>
2024-09-01 01:48:30 +0900

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import relativeLinks from "astro-relative-links";
+
+export default defineConfig({
+  site: 'https://piji333.github.io/dear',
+  integrations: [relativeLinks()]
+});
