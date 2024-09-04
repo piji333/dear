05449540fdc9581162a03d19e070e3d95446aa09
Phuoc <phamhbphuoc@gmail.com>
2024-09-05 01:50:34 +0900

    used netlify ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import relativeLinks from "astro-relative-links";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   site: 'https://piji333.github.io/dear',
   integrations: [relativeLinks()],
   output: "server",
+  adapter: netlify(),
 });
