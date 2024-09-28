173e1855b37458a10916d27a7c6ef523265dd1a2
Phuoc <phamhbphuoc@gmail.com>
2024-09-29 03:09:50 +0900

    init i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import relativeLinks from "astro-relative-links";
-
 import netlify from "@astrojs/netlify";
+import paraglide from "@inlang/paraglide-astro"
 
 export default defineConfig({
-  site: 'https://phuocnghi.live/wedding-invitation',
-  integrations: [relativeLinks()],
+  // Use astro's i18n routing for deciding which language to use
+  i18n: {
+    locales: ["en", "vi"],
+    defaultLocale: "en",
+  },
+  site: "https://phuocnghi.live/wedding-invitation",
+  integrations: [
+    relativeLinks(),
+    paraglide({
+      // recommended settings
+      project: "./project.inlang",
+      outdir: "./src/paraglide", //where your files should be
+    }),
+  ],
   output: "server",
   adapter: netlify(),
 });
